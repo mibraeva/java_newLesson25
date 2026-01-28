da8b7746bd09058524ad3a5cf13918cfb0107355
mibraeva <ibreaevamk12@gmail.com>
2026-01-28 13:01:57 +0900

    Tims's commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi there");
 
+        arr.forEach(functional(item) {
+            console.log(item);
+        });
+
     }
 }
