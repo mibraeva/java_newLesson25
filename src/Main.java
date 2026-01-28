e0c41c45df28540d0da7502ae6d8216ac6b88972
mibraeva <ibreaevamk12@gmail.com>
2026-01-28 13:00:11 +0900

    John's commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi there");
+
+        for(var i = 0; i < arr.length; i++) {
+            console.log(arr[i]);
+        }
 
     }
 }
